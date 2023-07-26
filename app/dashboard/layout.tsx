7c6b0beaa93f1629867769648853dca86f996f33
RealByter <justavishay@gmail.com>
2023-07-26 12:41:35 +0300

    feat: add the create board form

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,11 @@
-import Button from "@/components/Button";
+import CreateBoard from "@/components/CreateBoard";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto max-w-6xl p-8">
       <div className="flex justify-between">
         <h1 className="text-lg font-medium">All Boards</h1>
-        <Button>+ Add Board</Button>
+        <CreateBoard />
       </div>
       <div className="mt-8 grid grid-cols-[1fr] gap-4 min-[500px]:grid-cols-[repeat(2,1fr)] md:grid-cols-[repeat(3,1fr)] lg:grid-cols-[repeat(4,1fr)]">
         {children}
