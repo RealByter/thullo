fdcd9377723e0eded782e21d9f762132d4723907
RealByter <justavishay@gmail.com>
2023-07-27 13:34:21 +0300

    switch machine

--- components/CreateBoard.tsx
+++ components/CreateBoard.tsx
@@ -7,13 +7,18 @@ import Image from "next/image";
 import { LuGlobe2 } from "react-icons/lu";
 import ImageSearch from "./ImageSearch";
 import IconButton from "./IconButton";
+import Input from "./Input";
 
 export default function CreateBoard() {
   const [isOpen, setIsOpen] = useState(false);
+  const [title, setTitle] = useState("")
+  const [cover, setCover] = useState(
+    "https://images.unsplash.com/photo-1588421357574-87938a86fa28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+  );
 
   return (
     <>
-      <Button onClick={() => setIsOpen(true)}>+ Add Board</Button>
+      <Button onClick={() => setIsOpen(true)}><span className="mr-2">+</span> Add Board</Button>
       <Dialog
         open={isOpen}
         onClose={() => setIsOpen(false)}
@@ -24,19 +29,14 @@ export default function CreateBoard() {
         <div className="fixed inset-0 flex items-start justify-center p-4">
           <Dialog.Panel
             as="div"
-            className="mx-auto max-w-xs rounded-lg bg-white p-6 shadow-main mt-20"
+            className="mx-auto mt-20 max-w-xs rounded-lg bg-white p-6 shadow-main"
           >
             <div className="relative h-[100px] w-[260px] overflow-hidden rounded-lg">
-              <Image
-                fill
-                objectFit="cover"
-                src="https://images.unsplash.com/photo-1689702095156-135bdeb260e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                alt="Unsplash photo"
-              />
+              <Image fill objectFit="cover" src={cover} alt="" />
             </div>
-            <p>title</p>
+            <Input value={title}  />
             <div className="flex gap-5">
-              <ImageSearch setImage={() => {}} />
+              <ImageSearch setImage={setCover} />
               <IconButton icon={LuGlobe2} active={false}>
                 Public
               </IconButton>
