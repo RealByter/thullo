ebb9ceadea0d22f3b9a9c804796a73ed1ccc1bdf
RealByter <justavishay@gmail.com>
2023-08-03 00:09:19 +0300

    feat: add the github domain for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["lh3.googleusercontent.com", "images.unsplash.com", "localhost"] },
+  images: { domains: ["lh3.googleusercontent.com", "images.unsplash.com", "localhost", "avatars.githubusercontent.com"] },
   experimental: {
     serverActions: true,
   },
